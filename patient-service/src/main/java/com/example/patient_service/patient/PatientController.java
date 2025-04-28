114f6b9b48f753dbfc9c8a70cf36a1181706e775
mbuguag <mbuguajoseph498@gmail.com>
2025-04-28 17:45:12 +0300

    business logic and endpoints  patient-service

--- patient-service/src/main/java/com/example/patient_service/patient/PatientController.java
+++ patient-service/src/main/java/com/example/patient_service/patient/PatientController.java
@@ -1,4 +1,25 @@
 package com.example.patient_service.patient;
 
+import com.example.patient_service.dto.PatientResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/patients")
 public class PatientController {
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients(){
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
 }
--- patient-service/src/main/java/com/example/patient_service/patient/PatientService.java
+++ patient-service/src/main/java/com/example/patient_service/patient/PatientService.java
@@ -1,6 +1,7 @@
 package com.example.patient_service.patient;
 
 import com.example.patient_service.dto.PatientResponseDTO;
+import com.example.patient_service.mapper.PatientMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,5 +18,9 @@ public class PatientService {
     public List<PatientResponseDTO> getPatients(){
         List<Patient> patients = patientRepository.findAll();
 
+        return patients.stream()
+                .map( PatientMapper::toDTO)
+                .toList();
     }
 }
+
